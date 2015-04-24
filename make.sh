10619cb80805be45ed953c21961b42345e809c86
tsynik <tsynik@gmail.com>
2015-04-24 16:46:37 +0300

    add build targets to make.sh

--- make.sh
+++ make.sh
@@ -6,9 +6,17 @@ sudo ln -sf /usr/lib/libmpc.so.3 /usr/lib/libmpc.so.2
 export PATH=$PATH:/opt/hndtools-arm-linux-2.6.36-uclibc-4.5.3/bin
 export LANG=c
 cd $SRC
-echo " ---- Update sources from GIT ---- "
+# echo " ---- Update sources from GIT ---- "
 # git pull
 EXTENDNO=`git rev-parse --verify HEAD --short`
-make V1=git- V2=$EXTENDNO ws880z
-#make V1=$EXTENDNO ws880e
+# Huawei build
+#sed -ie "s|HUAWEI = 0|HUAWEI = 1|" $SRC/router/wwwAT/Makefile
+#sed -ie "s|XIAOMI = 1|XIAOMI = 0|" $SRC/router/wwwAT/Makefile
+#make V1=ML- V2=$EXTENDNO ws880e
+#make V1=ML- V2=$EXTENDNO ws880z
+#make V1=ML- V2=$EXTENDNO ws880zz
+# Xiaomi build
+sed -ie "s|HUAWEI = 1|HUAWEI = 0|" $SRC/router/wwwAT/Makefile
+sed -ie "s|XIAOMI = 0|XIAOMI = 1|" $SRC/router/wwwAT/Makefile
+make V1=ML- V2=$EXTENDNO r1dz
 

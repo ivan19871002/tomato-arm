71daec836e3b34aa778591d7cfda9ec5501a9dbb
tsynik <tsynik@gmail.com>
2016-04-04 21:04:15 +0300

    minor makefile update

--- make.sh
+++ make.sh
@@ -5,8 +5,7 @@ SRC=`pwd`/release/src-rt-6.x.4708
 # TOOLCHAIN:
 #
 sudo ln -sf $SRC/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/hndtools-arm-linux-2.6.36-uclibc-4.5.3
-#sudo mkdir -p /projects/hnd/tools/linux
-#sudo ln -sf $SRC/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /projects/hnd/tools/linux/hndtools-arm-linux-2.6.36-uclibc-4.5.3
+export PATH=$PATH:/opt/hndtools-arm-linux-2.6.36-uclibc-4.5.3/bin
 
 # SuSE x64 32bit libs for toolchain
 # sudo zypper install libelf1-32bit
@@ -15,7 +14,9 @@ sudo ln -sf $SRC/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/hndtools
 # Ubuntu 14.04 LTS x64 32bit libs for toolchain
 # sudo apt-get install libelf1:i386 zlib1g:i386
 
-export PATH=$PATH:/opt/hndtools-arm-linux-2.6.36-uclibc-4.5.3/bin
+# Build Essentials
+# sudo apt-get install build-essential gcc libncurses5 libncurses5-dev m4 bison flex libtool automake pkg-config
+
 export LANG=C
 export LC_ALL=en_US.UTF-8
 cd $SRC
@@ -48,7 +49,11 @@ make clean
 # Xiaomi build
 # make V1=$VER- V2=$EXTENDNO r1dz	# AIO Custom EX
 
-# Netgear build
+# Netgear r7000 initial build
 # make V1=$VER- V2=$EXTENDNO r7000init	# AIO initial
+
+# Asus build for RT-N18U, RT-AC56U, RT-AC68U, RT-AC68R, RT-AC68P
+# make V1=$VER- V2=$EXTENDNO ac68e	# VPN
+# make V1=$VER- V2=$EXTENDNO ac68z	# AIO
 
 # ETC...
